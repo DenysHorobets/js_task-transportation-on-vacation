9cec3484b846193fcdc34e899ee12fcedcfd742f
Denys <den.horobets@gmail.com>
2025-03-19 17:09:17 +0200

    Solution/task/transportation-on-vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costDay = 40;
+  let totalCost = costDay * days;
+
+  if (days >= 7) {
+    totalCost -= 50;
+  }
+
+  if (days >= 3 && days <= 6) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
